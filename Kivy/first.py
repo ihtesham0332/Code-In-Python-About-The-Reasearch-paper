fbe21df19ee65b3e73988f7512bd600fbf75a776
Muhammad Ihtesham <khanihtesham0332@gmail.come>
2024-07-02 08:48:50 +0500

    Tenserflow

---
+++ Kivy/first.py
@@ -0,0 +1,10 @@
+#make a Code for making the App in Python Using Kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MainWidget(Widget):
+    pass
+class first(App):
+    pass
+
+first().run()
